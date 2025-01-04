3546d56383e14487fac3fbd4a7539fd5d6fce0e9
fetisoff19 <fetisoff19@gmail.com>
2025-01-04 17:51:43 +0400

    fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,24 @@ module.exports = {
   root: true,
   env: {browser: true, es2020: true},
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@tanstack/eslint-plugin-query/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended'
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "react-hooks/rules-of-hooks": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "comma-style": ["error", "last"],
-    "semi": ["error", "never"],
-    "no-mixed-spaces-and-tabs": 0,
-    quotes: ["error", "single", {"allowTemplateLiterals": true}],
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-hooks/rules-of-hooks': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'comma-style': ['error', 'last'],
+    'semi': ['error', 'never'],
+    'no-mixed-spaces-and-tabs': 0,
+    quotes: ['error', 'single', {'allowTemplateLiterals': true}],
+    'react-refresh/only-export-components': [
+      'warn',
       {allowConstantExport: true},
     ],
   },
-};
+}
